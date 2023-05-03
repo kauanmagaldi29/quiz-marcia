8943d3e5ccf770003c89b9ab66ed80b6ae39b12d
Kauan Magaldi <kauan@MacBook-Pro-de-Kauan.local>
2023-05-03 09:31:30 -0300

    oi

---
+++ js/questions.js
@@ -0,0 +1,250 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "¿Cuál es el emoji que representa la tristeza extrema?",
+    answer: "😭",
+    options: [
+      "😢",
+      "😔",
+      "😥",
+      "😭"
+    ]
+  },
+    {
+    numb: 2,
+    question: "¿Qué significa el emoji 👀?",
+    answer: "Mirar",
+    options: [
+      "Ver",
+      "Mirar",
+      "Observar",
+      "Buscar"
+    ]
+  },
+    {
+    numb: 3,
+    question: "¿Cuál es el emoji que representa el amor?",
+    answer: "❤️",
+    options: [
+      "❤️",
+      "💕",
+      "💞",
+      "💘"
+    ]
+  },
+    {
+    numb: 4,
+    question: "¿Qué significa el emoji 🤔?",
+    answer: "Pensativo",
+    options: [
+      "a) Confundido",
+      "b) Pensativo",
+      "c) Sarcástico",
+      "d) Curioso"
+    ]
+  },
+    {
+    numb: 5,
+    question: "¿Qué significa el emoji 🤷‍♀️?",
+    answer: "No sé",
+    options: [
+      "a) No sé",
+      "b) Indeciso",
+      "c) Desinteresado",
+      "d) Ignorante"
+    ]
+  },
+
+  {
+    numb: 6,
+    question: "¿Cuál es el emoji que representa la sorpresa?",
+    answer: "😯",
+    options: [
+      "a) 😲",
+      "b) 😱",
+      "c) 😯",
+      "d) 😳"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "¿Qué significa el emoji 🤗?",
+    answer: "Abrazo",
+    options: [
+      "a) Abrazo",
+      "b) Felicidad",
+      "c) Agradecimiento",
+      "d) Amor"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "¿Cuál es el emoji que representa el miedo?",
+    answer: "😨",
+    options: [
+      "a) 😰",
+      "b) 😨",
+      "c) 😥",
+      "d) 😷"
+    ]
+  },
+  
+  {
+    numb: 9,
+    question: "¿Qué significa el emoji 🙏?",
+    answer: "Orar",
+    options: [
+      "a) Orar",
+      "b) Pedir ayuda",
+      "c) Agradecer",
+      "d) Lamentar"
+    ]
+  },
+  
+  {
+    numb: 10,
+    question: "¿Cuál es el emoji que representa la ira?",
+    answer: "😡",
+    options: [
+      "a) 😠",
+      "b) 💢",
+      "c) 😡",
+      "d) 🔥"
+    ]
+  },
+
+  {
+    numb: 11,
+    question: "¿Cuál es el emoji que representa la vergüenza?",
+    answer: "🙈",
+    options: [
+      "a) 😳",
+      "b) 🙈",
+      "c) 😣",
+      "d) 😫"
+    ]
+  },
+
+  {
+    numb: 12,
+    question: "¿Qué significa el emoji 💔?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "a) Soledad",
+      "b) Tristeza",
+      "c) Amor roto",
+      "d) Desamor"
+    ]
+  },
+
+  {
+    numb: 13,
+    question: "¿Qué significa el emoji 🤢?",
+    answer: "Náuseas",
+    options: [
+      "a) Náuseas",
+      "b) Mareo",
+      "c) Enfermedad",
+      "d) Mal humor"
+    ]
+  },
+
+  {
+    numb: 14,
+    question: "5. ¿Cuál es el emoji que representa la felicidad?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "a) 😀",
+      "b) 😊",
+      "c) 😃",
+      "d) 😄"
+    ]
+  },
+
+  {
+    numb: 15,
+    question: "5. ¿Cuál es el emoji que representa la felicidad?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "a) 😀",
+      "b) 😊",
+      "c) 😃",
+      "d) 😄"
+    ]
+  },
+
+  {
+    numb: 16,
+    question: "5. ¿Cuál es el emoji que representa la felicidad?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "a) 😀",
+      "b) 😊",
+      "c) 😃",
+      "d) 😄"
+    ]
+  },
+
+  {
+    numb: 17,
+    question: "5. ¿Cuál es el emoji que representa la felicidad?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "a) 😀",
+      "b) 😊",
+      "c) 😃",
+      "d) 😄"
+    ]
+  },
+
+  {
+    numb: 18,
+    question: "5. ¿Cuál es el emoji que representa la felicidad?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "a) 😀",
+      "b) 😊",
+      "c) 😃",
+      "d) 😄"
+    ]
+  },
+
+  {
+    numb: 19,
+    question: "5. ¿Cuál es el emoji que representa la felicidad?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "a) 😀",
+      "b) 😊",
+      "c) 😃",
+      "d) 😄"
+    ]
+  },
+
+  {
+    numb: 20,
+    question: "5. ¿Cuál es el emoji que representa la felicidad?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "a) 😀",
+      "b) 😊",
+      "c) 😃",
+      "d) 😄"
+    ]
+  },
+
+  {
+    numb: 21,
+    question: "5. ¿Cuál es el emoji que representa la felicidad?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "a) 😀",
+      "b) 😊",
+      "c) 😃",
+      "d) 😄"
+    ]
+  },
+];
