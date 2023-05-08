a7601b6c39267d1fd960da5750d58ede28841db4
kauanmagaldii <kauanmagaldi29@gmail.com>
2023-05-08 15:50:47 -0300

    final

--- js/questions.js
+++ js/questions.js
@@ -131,7 +131,7 @@ let questions = [
   {
     numb: 12,
     question: "¿Qué significa el emoji 💔?",
-    answer: "eXtensible Markup Language",
+    answer: "Desamor",
     options: [
       "Soledad",
       "Tristeza",
@@ -155,7 +155,7 @@ let questions = [
   {
     numb: 14,
     question: "¿Cuál es el emoji que representa la celebración?",
-    answer: "eXtensible Markup Language",
+    answer: "🎊",
     options: [
       " 🎉",
       "🎊",
@@ -191,60 +191,446 @@ let questions = [
   {
     numb: 17,
     question: "5. ¿Cuál es el emoji que representa la felicidad?",
-    answer: "eXtensible Markup Language",
+    answer: "😃",
     options: [
       "😀",
       "😊",
       "😃",
       "😄"
     ]
+  },
+  
+
+{
+numb: 18,
+question: "¿Qué significa el emoji 💩?",
+answer: "Excremento",
+options: [
+"Tristeza",
+"Enfermedad",
+"Felicidad",
+"Excremento"
+]
+},
+
+{
+numb: 19,
+question: "¿Qué significa el emoji 🙈?",
+answer: "No querer ver",
+options: [
+"Sorpresa",
+"Vergüenza",
+"Miedo",
+"No querer ver"
+]
+},
+
+{
+numb: 20,
+question: "¿Qué significa el emoji 😘?",
+answer: "Beso",
+options: [
+"Beso",
+"Abrazo",
+"Enamorado",
+"Decepción"
+]
+},
+
+{
+numb: 21,
+question: "¿Qué significa el emoji 🎉?",
+answer: "Celebración",
+options: [
+"Celebración",
+"Tristeza",
+"Enfermedad",
+"Aburrimiento"
+]
+},
+
+{
+numb: 22,
+question: "¿Qué significa el emoji 🤢?",
+answer: "Enfermedad",
+options: [
+"Enfermedad",
+"Tristeza",
+"Felicidad",
+"Hambre"
+]
+},
+
+{
+numb: 23,
+question: "¿Qué significa el emoji 😢?",
+answer: "Tristeza",
+options: [
+"Tristeza",
+"Alegría",
+"Confusión",
+"Miedo"
+]
+},
+
+{
+numb: 24,
+question: "¿Qué significa el emoji 🤷‍♂️?",
+answer: "Indiferencia",
+options: [
+"Indiferencia",
+"Confusión",
+"Desprecio",
+"Sorpresa"
+]
+},
+
+{
+numb: 25,
+question: "¿Qué significa el emoji 🤮?",
+answer: "Asco",
+options: [
+"Asco",
+"Alegría",
+"Desagrado",
+"Hambre"
+]
+},
+
+{
+numb:26,
+question: "¿Qué significa el emoji 🥺?",
+answer: "Tristeza",
+options: [
+"Tristeza",
+"Amor",
+"Vergüenza",
+"Enfermedad"
+]
+},
+
+{
+numb: 27,
+question: "¿Qué significa el emoji 🤯?",
+answer: "Miedo",
+options: [
+"Sorpresa",
+"Felicidad",
+"Enfermedad",
+"Miedo"
+]
+},
+
+{
+numb: 28,
+question: "¿Qué significa el emoji 🤪?",
+answer: "Locura",
+options: [
+"Locura",
+"Tristeza",
+"Confusión",
+"Desagrado"
+]
+},
+
+{
+numb: 29,
+question: "¿Qué significa el emoji 😒?",
+answer: "Descontento",
+options: [
+"Descontento",
+"Alegría",
+"Indiferencia",
+"Miedo"
+]
+},
+
+{
+  numb: 301,
+  question: "¿Qué significa el emoji 🥑?",
+  answer: "Aguacate",
+  options: [
+  "Tomate",
+  "Lechuga",
+  "Cebolla",
+  "Aguacate"
+  ]
+  },
+  
+  {
+  numb: 31,
+  question: "¿Qué significa el emoji 🥰?",
+  answer: "Amor",
+  options: [
+  "Felicidad",
+  "Tristeza",
+  "Miedo",
+  "Amor"
+  ]
+  },
+  
+  {
+  numb: 32,
+  question: "¿Qué significa el emoji 🤝?",
+  answer: "Apretón de manos",
+  options: [
+  "Apretón de manos",
+  "Abrazo",
+  "Saludo",
+  "Despedida"
+  ]
+  },
+  
+  {
+  numb: 33,
+  question: "¿Qué significa el emoji 🍁?",
+  answer: "Hoja de arce",
+  options: [
+  "Flor",
+  "Hoja de palma",
+  "Hoja de arce",
+  "Hojas secas"
+  ]
+  },
+  
+  {
+  numb: 34,
+  question: "¿Qué significa el emoji 🌼?",
+  answer: "Flor",
+  options: [
+  "Flor",
+  "Árbol",
+  "Pasto",
+  "Planta"
+  ]
+  },
+  
+  {
+  numb: 35,
+  question: "¿Qué significa el emoji 🌊?",
+  answer: "Olas",
+  options: [
+  "Río",
+  "Lago",
+  "Mar",
+  "Olas"
+  ]
+  },
+  
+  {
+  numb: 36,
+  question: "¿Qué significa el emoji 🍓?",
+  answer: "Fresa",
+  options: [
+  "Manzana",
+  "Plátano",
+  "Naranja",
+  "Fresa"
+  ]
+  },
+  
+  {
+  numb: 37,
+  question: "¿Qué significa el emoji 🍔?",
+  answer: "Hamburguesa",
+  options: [
+  "Hot dog",
+  "Pizza",
+  "Sándwich",
+  "Hamburguesa"
+  ]
+  },
+  
+  {
+  numb: 38,
+  question: "¿Qué significa el emoji 🎶?",
+  answer: "Notas musicales",
+  options: [
+  "Banda",
+  "Cantante",
+  "Instrumento",
+  "Notas musicales"
+  ]
+  },
+  
+  {
+  numb: 39,
+  question: "¿Qué significa el emoji 🏠?",
+  answer: "Casa",
+  options: [
+  "Apartamento",
+  "Oficina",
+  "Hotel",
+  "Casa"
+  ]
   },
 
   {
-    numb: 18,
-    question: "5. ¿Cuál es el emoji que representa la felicidad?",
-    answer: "eXtensible Markup Language",
-    options: [
-      "😀",
-      "😊",
-      "😃",
-      "😄"
-    ]
+  numb: 40,
+  question: "¿Qué significa el emoji 🚀?",
+  answer: "Cohete",
+  options: [
+  "Avión",
+  "Auto",
+  "Barco",
+  "Cohete"
+  ]
+  },
+  
+  {
+  numb: 41,
+  question: "¿Qué significa el emoji 🍟?",
+  answer: "Papas fritas",
+  options: [
+  "Papas fritas",
+  "Puré de papas",
+  "Ensalada",
+  "Arroz"
+  ]
   },
 
   {
-    numb: 19,
-    question: "5. ¿Cuál es el emoji que representa la felicidad?",
-    answer: "eXtensible Markup Language",
+    numb: 42,
+    question: "¿Qué significa el emoji 🤔?",
+    answer: "Pensando",
     options: [
-      "😀",
-      "😊",
-      "😃",
-      "😄"
+    "Durmiendo",
+    "Enfadado",
+    "Riendo",
+    "Pensando"
     ]
-  },
+    },
+    
+    {
+    numb: 43,
+    question: "¿Qué significa el emoji 🕰️?",
+    answer: "Reloj de arena",
+    options: [
+    "Reloj de pared",
+    "Reloj de pulsera",
+    "Reloj digital",
+    "Reloj de arena"
+    ]
+    },
+    
+    {
+    numb:44,
+    question: "¿Qué significa el emoji 🎂?",
+    answer: "Pastel de cumpleaños",
+    options: [
+    "Pastel de bodas",
+    "Pastel de chocolate",
+    "Torta",
+    "Pastel de cumpleaños"
+    ]
+    },
+    
+    {
+    numb: 45,
+    question: "¿Qué significa el emoji 📷?",
+    answer: "Cámara",
+    options: [
+    "Teléfono",
+    "Tableta",
+    "Laptop",
+    "Cámara"
+    ]
+    },
+    
+    {
+    numb: 46,
+    question: "¿Qué significa el emoji 🍩?",
+    answer: "Rosquilla",
+    options: [
+    "Pan",
+    "Pretzel",
+    "Galleta",
+    "Rosquilla"
+    ]
+    },
+    
+    {
+    numb: 47,
+    question: "¿Qué significa el emoji 🎥?",
+    answer: "Cámara de cine",
+    options: [
+    "Televisión",
+    "Computadora",
+    "Cámara de fotos",
+    "Cámara de cine"
+    ]
+    },
+    
+    {
+    numb: 48,
+    question: "¿Qué significa el emoji 🌵?",
+    answer: "Cactus",
+    options: [
+    "Árbol",
+    "Flor",
+    "Planta carnívora",
+    "Cactus"
+    ]
+    },
+    
+    {
+    numb:49,
+    question: "¿Qué significa el emoji 🍇?",
+    answer: "Uvas",
+    options: [
+    "Manzanas",
+    "Naranjas",
+    "Fresas",
+    "Uvas"
+    ]
+    },
+    
+    {
+    numb: 50,
+    question: "¿Qué significa el emoji 🚲?",
+    answer: "Bicicleta",
+    options: [
+    "Auto",
+    "Motocicleta",
+    "Camión",
+    "Bicicleta"
+    ]
+    },
+    
+    {
+    numb: 51,
+    question: "¿Qué significa el emoji 🍉?",
+    answer: "Sandía",
+    options: [
+    "Melón",
+    "Piña",
+    "Manzana",
+    "Sandía"
+    ]
+    },
+    
+    {
+    numb: 52,
+    question: "¿Qué significa el emoji 🐶?",
+    answer: "Perro",
+    options: [
+    "Gato",
+    "Conejo",
+    "Ratón",
+    "Perro"
+    ]
+    },
+    
+    {
+    numb: 53,
+    question: "¿Qué significa el emoji 🌹?",
+    answer: "Rosa",
+    options: [
+    "Girasol",
+    "Lirio",
+    "Margarita",
+    "Rosa"
+    ]
+    },
 
-  {
-    numb: 20,
-    question: "5. ¿Cuál es el emoji que representa la felicidad?",
-    answer: "eXtensible Markup Language",
-    options: [
-      "😀",
-      "😊",
-      "😃",
-      "😄"
-    ]
-  },
-
-  {
-    numb: 21,
-    question: "5. ¿Cuál es el emoji que representa la felicidad?",
-    answer: "eXtensible Markup Language",
-    options: [
-      "😀",
-      "😊",
-      "😃",
-      "😄"
-    ]
-  },
 ];
